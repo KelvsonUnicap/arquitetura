0cf999f270ddb6bcd4ee50022ac34e60448e0f50
Kelvson Nilson <kelvsonnilson01@gmail.com>
2026-05-01 23:27:01 -0300

    chore(util): add http order constant path

--- orchestration/src/main/java/com/kelvson/orchestration/util/AppConstants.java
+++ orchestration/src/main/java/com/kelvson/orchestration/util/AppConstants.java
@@ -5,4 +5,5 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class AppConstants {
     public static final String ORDER_BASE_PATH = "/orders";
+    public static final String ORDER_SERVICE_URL = "http://order-service:8080/orders";
 }
