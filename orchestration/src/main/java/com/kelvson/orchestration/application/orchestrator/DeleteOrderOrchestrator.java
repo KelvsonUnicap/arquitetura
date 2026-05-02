15205e15e036d1121ddf0a8f6f61be1b2915bbf6
Kelvson Nilson <kelvsonnilson01@gmail.com>
2026-05-02 11:05:46 -0300

    feat(orchestrator): add DeleteOrderOrchestrator
    - implement DeleteOrderUseCase

---
+++ orchestration/src/main/java/com/kelvson/orchestration/application/orchestrator/DeleteOrderOrchestrator.java
@@ -0,0 +1,22 @@
+package com.kelvson.orchestration.application.orchestrator;
+
+import com.kelvson.orchestration.application.usecase.DeleteOrderUseCase;
+import com.kelvson.orchestration.infrastructure.client.OrderClient;
+import com.kelvson.orchestration.infrastructure.client.PaymentClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DeleteOrderOrchestrator implements DeleteOrderUseCase {
+
+    private final OrderClient orderClient;
+    private final PaymentClient paymentClient;
+
+    @Override
+    public void execute(Long orderId) {
+        if(orderClient.exists(orderId)){
+            orderClient.delete(orderId);
+        }
+    }
+}
