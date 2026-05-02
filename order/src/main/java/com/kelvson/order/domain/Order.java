b49725a5361c8fae5805f90bd291e1c479330c09
Kelvson Nilson <kelvsonnilson01@gmail.com>
2026-05-02 17:34:37 -0300

    refactor(domain): add @CreationTimestamp and @UpdateTimestamp annotation
    - add public constructor with 3 arguments: restaurantId, customerId and totalAmount; defined status as PENDING in constructor
    
    - add @Column annotation in Order attributes. (We can't use @ManyToOne 'cause we're making a distributed system, then cannot use @ManyToOne, only @Column.

--- order/src/main/java/com/kelvson/order/domain/Order.java
+++ order/src/main/java/com/kelvson/order/domain/Order.java
@@ -4,28 +4,47 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
 @Entity
+@Table(name="orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name="restaurant_id")
     private Long restaurantId;
 
+    @Column(name="customer_id")
     private Long customerId;
 
     @Setter
     @Enumerated(EnumType.STRING)
     private Status status;
 
+    @Column(name="total_amount")
     private Double totalAmount;
 
-    private LocalDateTime CreatedAt;
+    @CreationTimestamp
+    @Column(name="created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name="updated_at")
+    private LocalDateTime updatedAt;
+
+    public Order(Long restaurantId, Long customerId, Double totalAmount){
+        this.restaurantId = restaurantId;
+        this.customerId = customerId;
+        this.totalAmount = totalAmount;
+        this.status = Status.PENDING;
+    }
 
     public void nextStatus(){
         status.next(this);
