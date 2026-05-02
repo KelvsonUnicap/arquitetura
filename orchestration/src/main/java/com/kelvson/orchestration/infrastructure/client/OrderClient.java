bc26c6559e8831ba953c9e98f9bf30fb10551f65
Kelvson Nilson <kelvsonnilson01@gmail.com>
2026-05-01 23:23:03 -0300

    feat(infrastructure): add exists method in OrderClient

--- orchestration/src/main/java/com/kelvson/orchestration/infrastructure/client/OrderClient.java
+++ orchestration/src/main/java/com/kelvson/orchestration/infrastructure/client/OrderClient.java
@@ -1,6 +1,7 @@
 package com.kelvson.orchestration.infrastructure.client;
 
 import com.kelvson.orchestration.interfaces.dto.CreateOrderRequest;
+import com.kelvson.orchestration.util.AppConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -16,22 +17,31 @@ public class OrderClient {
 
     public Long create(CreateOrderRequest request){
         return restTemplate.postForObject(
-            "http://order-service:8080/orders",
+                AppConstants.ORDER_BASE_HTTP_PATH,
                 request,
                 Long.class
         );
     }
 
+    public boolean exists(Long orderId) {
+        return Boolean.TRUE.equals(
+                restTemplate.getForObject(
+                        AppConstants.ORDER_BASE_HTTP_PATH + orderId + "/exists",
+                        Boolean.class
+                )
+        );
+    }
+
     public void confirm(Long orderId){
         restTemplate.postForLocation(
-          "http://order-service:8080/orders/" + orderId + "/confirm",
+          AppConstants.ORDER_BASE_HTTP_PATH + orderId + "/confirm",
             null
         );
     }
 
     public void cancel(Long orderId){
         restTemplate.postForLocation(
-            "http://order-service:8080/orders/" + orderId + "/cancel",
+            AppConstants.ORDER_BASE_HTTP_PATH + orderId + "/cancel",
             null
         );
     }
