7bde60ba2dbe24fa488bd34c05b8b16168ebb26b
Kelvson Nilson <kelvsonnilson01@gmail.com>
2026-05-02 22:43:51 -0300

    feat(service): add OrderService

---
+++ order/src/main/java/com/kelvson/order/service/OrderService.java
@@ -0,0 +1,22 @@
+package com.kelvson.order.service;
+
+import com.kelvson.order.domain.Order;
+import com.kelvson.order.dto.CreateOrderRequest;
+import com.kelvson.order.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderService(@Autowired OrderRepository orderRepository){
+        this.orderRepository = orderRepository;
+    }
+
+    public Long save(CreateOrderRequest request){
+        Order order = new Order(request.restaurantId(), 1L, request.amount());
+        return orderRepository.save(order).getId();
+    }
+}
